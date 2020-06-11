92d0839d1f6c2ad673f13189c21e5d306c367dee
Saklain1704032 <61911332+Saklain1704032@users.noreply.github.com>
2020-06-11 15:01:04 +0600

    Create slogin.php

---
+++ slogin.php
@@ -0,0 +1,65 @@
+<!doctype html>
+  <html lang="en">
+    <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+      <title>NSA|Courses</title>
+    </head>
+    <body>
+      <div class="container">
+          <h2 >Take A Glance At Our Available Courses</h2>
+      </div>
+     <div class="container" style="margin-top:30px">
+         <div class="row" style=" height:350px;margin-bottom:30px;background-color:#66b3ff; border-radius:20px;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 5), 0 10px 100px 0 rgba(0, 0, 0, 5);">
+              <div class="col-sm-6">
+                    <img src="ai4.png" alt="" style="border-radius:20px;height:350px">
+              </div>
+              <div class="col-sm-4" style="color:white">
+                  <h2>Artificial Intelligence</h2>
+                  <p> Become an AI expert with the help of our talented minds!!!Machine learning, modeling, and simulation: engineering problem-solving in the age of AI. Begins April 2020. Register today! Professional Certificate. Networking Opportunities. Learn Online. Earn CEUs. Collaborative Learning. Access MIT Content. MIT Faculty Led.</p>
+                  <form action="aivideo.html">
+                  <button style="margin-bottom:5px "type="submit" class="btn btn-dark" name="button">SEE PREVIEW</button>
+                  </form>
+                  <button type="button" class="btn btn-dark" name="button">Fee: $500</button>
+              </div>
+         </div>
+         <div class="row" style="background-color:#000033;margin-bottom:30px; border-radius:20px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 5), 0 10px 100px 0 rgba(0, 0, 0, 5);">
+              <div class="col-sm-6">
+                    <img src="ml.png" alt="" style="border-radius:20px;overflow:hidden">
+              </div>
+              <div class="col-sm-4" style="color:white">
+                  <h2>MAchine Learning</h2>
+                  <p> Become an AI expert with the help of our talented minds!!!Machine learning, modeling, and simulation: engineering problem-solving in the age of AI. Begins April 2020. Register today! Professional Certificate. Networking Opportunities. Learn Online. Earn CEUs. Collaborative Learning. Access MIT Content. MIT Faculty Led.</p>
+                  <form action="mlvideo.html">
+                  <button style="margin-bottom:5px " type="submit" class="btn btn-dark" name="button">SEE PREVIEW</button>
+                  </form>
+                  <button type="button"  class="btn btn-dark" name="button">Fee: $500</button>
+              </div>
+         </div>
+         <div class="row" style="background-color:#4d0000;margin-bottom:30px; border-radius:20px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 5), 0 10px 100px 0 rgba(0, 0, 0, 5);">
+              <div class="col-sm-6">
+                    <img src="sa.jpg" alt="" style="border-radius:20px;overflow:hidden">
+              </div>
+              <div class="col-sm-4" style="color:white">
+                  <h2>System Analyst</h2>
+                  <p>Systems analysts may serve as change agents who identify the organizational improvements needed, design systems to implement those changes, and train and motivate others to use the systems.</p>
+                  <form action="aivideo.html">
+                  <button style="margin-bottom:5px " type="submit" class="btn btn-dark" name="button">SEE PREVIEW</button>
+                  </form>
+                  <button type="button"  class="btn btn-dark" name="button">Fee: $500</button>
+              </div>
+         </div>
+     </div>
+
+      <!-- Optional JavaScript -->
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    </body>
+  </html>
